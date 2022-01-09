fa3e0a5f3553acc59b9d1b8fd44f25966b70c2cc
Yurowa Antonina <antonina.yurowa@yandex.ru>
2022-01-09 21:35:35 +0300

    Задачи 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,20 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0
+};
+calculator.read = function (a, b) {
+  calculator.a = a;
+  calculator.b = b;
+};
+calculator.sum = function () {
+  let result = 0;
+  result = calculator.a + calculator.b
+  return result
+};
+calculator.mul = function () {
+  let result = 0;
+  result = calculator.a * calculator.b
+  return result
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
